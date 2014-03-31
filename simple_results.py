76f009c5d9d17a6ccb11c8fbe22f56f87eca9d88
Michael Wood-Vasey <wmwv@pitt.edu>
2014-03-31 14:17:10 -0400

    Try plotting.

--- simple_results.py
+++ simple_results.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 
 from astropy.io import fits
+import numpy as np
+import pylab as plt
+
 file='result.fits'
 
 data=fits.getdata(file,1)
 
-py.plot(data['g']-data['r'], data['u'], 'o')
-py.xlabel('g-r [mag]')
-py.ylabel('u [mag]')
+def plot_u_gr(u, g, r):
+    """Plot u vs. g-r for sample SDSS data
+
+    Input: u, g, r
+    Output: Displays plot of u vs. g-r to screen
+""" 
+    py.plot(g-r, u, 'o')
+    py.xlabel('g-r [mag]')
+    py.ylabel('u [mag]')
 
