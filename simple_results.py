1c155439c0b72c075a415c089e67ae54de4efc0d
Michael Wood-Vasey <wmwv@pitt.edu>
2014-03-31 14:24:32 -0400

    Adds show to plot function.  Adds plot to main script.

--- simple_results.py
+++ simple_results.py
@@ -4,17 +4,21 @@ from astropy.io import fits
 import numpy as np
 import pylab as plt
 
-file='result.fits'
-
-data=fits.getdata(file,1)
-
 def plot_u_gr(u, g, r):
     """Plot u vs. g-r for sample SDSS data
 
     Input: u, g, r
     Output: Displays plot of u vs. g-r to screen
 """ 
-    py.plot(g-r, u, 'o')
-    py.xlabel('g-r [mag]')
-    py.ylabel('u [mag]')
+    plt.plot(g-r, u, 'o')
+    plt.xlabel('g-r [mag]')
+    plt.ylabel('u [mag]')
+    plt.show()
+
+if __name__ == "__main__":
+    file='result.fits'
+
+    data=fits.getdata(file,1)
+
+    plot_u_gr(data['u'], data['g'], data['r'])
 
